1940a4743558ea3e3d4980bb15718faf8e7dd6e7
tomzarhin <tomzarhin@gmail.com>
2019-11-07 21:26:28 +0200

    database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
-
+mongoose.connect("mongodb+srv://admin:admin@cluster0.mongodb.net:27017/test");
+console.log(mongoose);
 module.exports = {mongoose};
