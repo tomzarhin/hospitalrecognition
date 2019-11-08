c2735c3de40dc677e9aa894bf4c448f3ddcf26bb
tomzarhin <tomzarhin@gmail.com>
2019-11-08 20:38:36 +0200

    working mongodb atlas

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,5 +5,4 @@ mongoose.Promise = global.Promise;
 
 //change the database with yours
 mongoose.connect("mongodb+srv://admin:admin@cluster0-gyzrb.azure.mongodb.net/hospitaldb?retryWrites=true&w=majority");
-console.log(mongoose);
 module.exports = {mongoose};
