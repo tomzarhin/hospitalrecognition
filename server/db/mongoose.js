2032a0ba4a7ec688fe54ef04b4224c1dc354b331
tomzarhin <tomzarhin@gmail.com>
2019-11-08 20:25:28 +0200

    my own database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://localhost:27017/hospitaldb");
+mongoose.connect("mongodb+srv://admin:admin@cluster0-gyzrb.azure.mongodb.net/test?retryWrites=true&w=majority/hospitaldb");
 console.log(mongoose);
 module.exports = {mongoose};
