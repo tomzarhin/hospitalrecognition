b9b48d59639935289b6d5be224bce7e19eb82779
tomzarhin <tomzarhin@gmail.com>
2019-11-08 20:28:52 +0200

    database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb+srv://admin:admin@cluster0-gyzrb.azure.mongodb.net/test?retryWrites=true&w=majority/hospitaldb");
+mongoose.connect("mongodb+srv://admin:admin@cluster0-gyzrb.azure.mongodb.net/hospitaldb?retryWrites=true&w=majority");
 console.log(mongoose);
 module.exports = {mongoose};
